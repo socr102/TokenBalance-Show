9b14573fb01e0469d2e04fad3ed68b86f2f880ee
socr102 <normanburtonfree@gmail.com>
2022-06-18 04:37:14 +0900

    final DApp

--- src/App.js
+++ src/App.js
@@ -1,106 +1,90 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import Input from './Address_Balance.txt';
 
-function App() {
+const contractABI = require("./contract-abi.json");
+const contractAddress = "0xDBC99496c826540419d08753695A885039FC6776";
 
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState(null);
-  const [userAddress, setUserAddress] = useState("");
-  const [amount, setAmount] = useState();
-  const [toaddress, setToaddress] = useState("address-1");
+ const App = () => {
 
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", accountsChanged);
-      window.ethereum.on("chainChanged", chainChanged);
+  const[accountBal, setaccountBal] = useState([]);
+  const[contractBal, setcontractBal] = useState();
+  const [claimableAddress_Array, setclaimableAddress_Array] = useState([]);
+  const [walletAddress, setWalletAddress] = useState();
+  const [amount, setAmount] = useState(0);
+  const [accountSigner, setSigner] = useState();
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const testContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+  useEffect(()=>{
+    fetch(Input)
+      .then((r) => r.text())
+      .then(text  => {
+        const lines = text.split("\n");
+        for(var line = 0; line < lines.length; line ++){
+          if(lines[line]){
+            const claimableAddress = lines[line].slice(0,42);
+            setclaimableAddress_Array(element => [claimableAddress, ...element])
+          }
+        }
+    })
+  },[])
+
+  useEffect(()=>{
+
+    if(claimableAddress_Array.length !=0){
+      loadContractBalance();
+      loadUserBalance();
     }
-  }, []);
 
-  const connectHandler = async () => {
-    if (window.ethereum) {
-      try {
-        const res = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        await accountsChanged(res[0]);
-        const { ethereum } = window;
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        
-        const addr = await signer.getAddress();
+  });
 
-        setUserAddress(addr.toString());
-      } catch (err) {
-        console.error(err);
-        setErrorMessage("There was a problem connecting to MetaMask");
-      }
-    } else {
-      setErrorMessage("Install MetaMask");
-    }
-  };
-
-  const accountsChanged = async (newAccount) => {
-    setAccount(newAccount);
-    try {
-      const balance = await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [newAccount.toString(), "latest"],
-      });
-      setBalance(ethers.utils.formatEther(balance));
-    } catch (err) {
-      console.error(err);
-      setErrorMessage("There was a problem connecting to MetaMask");
-    }
-  };
-
-  const chainChanged = () => {
-    setErrorMessage(null);
-    setAccount(null);
-    setBalance(null);
-  };
-
-  // useEffect(() => {
-  //   connectHandler()
-  // },[]);
-
-  const getInputValue = (event)=>{
-    // show the user input value to console
-    const userValue = event.target.value;
-    setAmount(userValue);
-    
-  };
-
-
-  const claimHandle = () => {
-    console.log(amount);
-    console.log(toaddress);
+  const loadContractBalance = async () => {
+    let contractBalance = await testContract.balanceOf(contractAddress);
+    setcontractBal(Number(contractBalance)/Math.pow(10,18))
+  }
+  
+  const loadUserBalance = async () => {    
+    let userBal1 = await testContract.balanceOf(claimableAddress_Array[0]);
+    let userBal2 = await testContract.balanceOf(claimableAddress_Array[1]);
+    let userBal3 = await testContract.balanceOf(claimableAddress_Array[2]);
+    let userBal4 = await testContract.balanceOf(claimableAddress_Array[3]);
+    let userBal5 = await testContract.balanceOf(claimableAddress_Array[4]);
+    setaccountBal([Number(userBal1)/Math.pow(10,18),Number(userBal2)/Math.pow(10,18),Number(userBal3)/Math.pow(10,18),Number(userBal4)/Math.pow(10,18),Number(userBal5)/Math.pow(10,18)])
   }
 
+  const btnhandler = async() => {
+    var metamaskProvider = window.ethereum;
+		await metamaskProvider.request({ method: 'eth_requestAccounts' });
+		const provider = new ethers.providers.Web3Provider(metamaskProvider);
+    const signer = provider.getSigner();
+    const walletAddress = await signer.getAddress();
+    setWalletAddress(walletAddress);
+    setSigner(signer);
+  };
+ 
+  const claimHandler = async () => {
+    const testContract = new ethers.Contract(contractAddress, contractABI, accountSigner);
+    await testContract.claim(ethers.utils.parseEther(amount.toString()));
+  }
   return (
     <div className="App">
-      <div>
-        <div className='address-balance'>
-          <select className='address' onChange={(e) => setToaddress(e.target.value)}>
-            <option value="address-1">address-1</option>
-            <option value="address-2">address-2</option>
-            <option value="address-3">address-3</option>
-            <option value="address-4">address-4</option>
-            <option value="address-5">address-5</option>
-          </select>
-          <div className='balance'>balance: <span>100</span></div>
-        </div>
-        <div className='claim-connect'>
-          <input type="text" className="amount" onChange={getInputValue}/>
-          {
-            account != null ? <button className='btn-claim-enable' onClick={claimHandle}>claim</button> : <button className='btn-claim-disabled' disabled>claim</button>
-          }
-          
-        </div>
-        {
-          account == null ? <button className='btn-connectwallet' onClick={connectHandler}>connect metamask</button> : <button className='btn-connectwallet' onClick={connectHandler}>{userAddress}</button>
-        }
+      <button className="btn-connectwallet" onClick={btnhandler}>{walletAddress ? walletAddress : "Connect Wallet"}</button>
+      <div className="claimableAddress">
+        <h1>Claimable Addresses and their balances:</h1>
+        <p>{claimableAddress_Array[0]}: <span> {accountBal[0]}</span></p>
+        <p>{claimableAddress_Array[1]}: <span> {accountBal[1]}</span></p>
+        <p>{claimableAddress_Array[2]}: <span> {accountBal[2]}</span></p>
+        <p>{claimableAddress_Array[3]}: <span> {accountBal[3]}</span></p>
+        <p>{claimableAddress_Array[4]}: <span> {accountBal[4]}</span></p>
+        <h1>Current Smart Contract Balance:</h1>
+        <p className="contractBalance">{contractBal}</p>
+      </div>
+      <div className="claimPart">
+        <input type="number" className="claimAmount" value={amount} onChange={e => setAmount(e.target.value)}  placeholder="Input Claim Amount"/>
+        <button className="btn-claim-enable" onClick={claimHandler}>Claim</button>
       </div>
     </div>
   );
